1b2c77b1048fb5ae5939eaae88608059e0d7102a
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-11-13 11:12:46 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan50/energikinetik/EnergiKinetik.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan50.energikinetik;
+
+/**
+ *
+* @author RizalSihombing
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk cara menghitung energi kinetik
+ */
+public class EnergiKinetik {
+    private double massa, kecepatan;
+    
+    public double getMassa() {
+        return this.massa / 1000;
+    }
+    
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+    
+    public double getKecepatan() {
+        return this.kecepatan;
+    }
+    
+    public void setKecepatan(double kecepatan) {
+        this.kecepatan = kecepatan;
+    }
+    
+    public double hitungEnergiKinetik(double massa, double kecepatan) {
+        return 0.5 * massa * Math.pow(kecepatan, 2);
+    }
+    
+    public double hitungUsaha(double kecepatanAwal) {
+        return 0.5 * massa * (Math.pow(kecepatan, 2) - Math.pow(kecepatanAwal, 2));
+    }
+}
---
+++ src/if1/pkg10119028/latihan50/energikinetik/IF110119028Latihan50EnergiKinetik.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan50.energikinetik;
+
+/**
+ *
+ * @author RizalSihombing
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk cara menghitung energi kinetik
+ */
+public class IF110119028Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EnergiKinetik energiKinetik = new EnergiKinetik();
+        
+        energiKinetik.setMassa(145.0);
+        energiKinetik.setKecepatan(25.0);
+        double massa = energiKinetik.getMassa();
+        double kecepatan = energiKinetik.getKecepatan();
+        System.out.println("Sebuah bola baseball dengan massa " + massa + " g dilempar dengan kecepatan " + kecepatan + " m/s");
+        System.out.println("a.\t Berapakah energi kinetiknya ? " + energiKinetik.hitungEnergiKinetik(massa, kecepatan));
+        System.out.println("b.\t Berapakah usaha yang dilakukan pada bola untuk mencapai kecepatan ini, jika dimulai dari keadaan diam? " + energiKinetik.hitungUsaha(0));
+        System.out.println("Alasan : Karena energi kinetik awal adalah nol, maka usaha total yang dilakukan sama dengan energi kinetik akhir, yaitu 45 joule.");
+    }
+    
+}
